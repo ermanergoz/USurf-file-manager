12f3e10f9ba82a4d3a1da0b8de56531aaff07516
Yusuf Erman ERGÖZ <ermanergoz>
2019-11-20 21:39:10 +0100

    Added functionality to open the directories.

--- app/src/main/java/com/erman/drawerfm/activities/FragmentActivity.kt
+++ app/src/main/java/com/erman/drawerfm/activities/FragmentActivity.kt
@@ -1,6 +1,7 @@
 package com.erman.drawerfm.activities
 
 import DirectoryData
+import ListDirFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,14 +9,12 @@ import com.erman.drawerfm.R
 
 class FragmentActivity : AppCompatActivity(), ListDirFragment.OnItemClickListener {
     lateinit var path: String
+    private lateinit var filesListFragment: ListDirFragment
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
+    private fun launchFragment(path: String) {
+        this.supportFragmentManager.popBackStack()
 
-        this.path = intent.getStringExtra("path")
-
-        var filesListFragment = ListDirFragment.buildFragment(path)
+        filesListFragment = ListDirFragment.buildFragment(path)
 
         supportFragmentManager.beginTransaction()
             .add(R.id.fragmentContainer, filesListFragment)
@@ -23,20 +22,29 @@ class FragmentActivity : AppCompatActivity(), ListDirFragment.OnItemClickListene
             .commit()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+
+        this.path = intent.getStringExtra("path")
+
+        launchFragment(path)
+    }
+
     override fun onClick(directoryData: DirectoryData) {
         if (directoryData.isFolder) {
-            Log.e("clicked item is a", "folder")
+            Log.e("path of clicked item is", directoryData.path)
         } else {
-            Log.e("clicked item is a", "file")
+            Log.e("path of clicked item is", directoryData.path)
         }
+        launchFragment(directoryData.path)
     }
 
     override fun onLongClick(directoryData: DirectoryData) {
         Log.e("item is", "long clicked")
-
     }
 
-    override fun onBackPressed() {
-        finish()
-    }
+    /* override fun onBackPressed() {
+         finish()
+     }*/
 }
