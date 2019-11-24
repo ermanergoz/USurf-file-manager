6a043354d0d589dd9d8f2ff18770a3f56b0e71bb
Yusuf Erman ERGÖZ <ermanergoz>
2019-11-24 20:25:28 +0100

    Added about DrawerFM dialog

---
+++ app/src/main/java/com/erman/drawerfm/dialogs/AboutDrawerFMDialog.kt
@@ -0,0 +1,40 @@
+package com.erman.drawerfm.dialogs
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import com.erman.drawerfm.R
+
+
+class AboutDrawerFMDialog() : DialogFragment() {
+
+    private lateinit var linkText: TextView
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+
+            val inflater = requireActivity().layoutInflater
+            val dialogView: View = inflater.inflate(R.layout.about_drawerfm_dialog, null)
+            val builder = AlertDialog.Builder(it)
+
+            this.linkText = dialogView.findViewById(R.id.linkText)
+
+            var link = "To check the source code:\n" + "https://github.com/ermanergoz/DraverFM"
+            linkText.text = link
+
+            builder.setMessage(R.string.about)
+
+                .setPositiveButton(R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        getDialog()?.cancel()
+                    })
+
+            builder.setView(dialogView)
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
