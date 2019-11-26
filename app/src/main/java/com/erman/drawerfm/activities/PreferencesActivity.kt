69781a5beebc30bd995f94121c72fcc9385497c9
Yusuf Erman ERGÖZ <ermanergoz>
2019-11-26 17:53:00 +0100

    Implemented themes

--- app/src/main/java/com/erman/drawerfm/activities/PreferencesActivity.kt
+++ app/src/main/java/com/erman/drawerfm/activities/PreferencesActivity.kt
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.erman.drawerfm.R
 import com.erman.drawerfm.fragments.PreferencesFragment
 
-
 class PreferencesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_preferences)
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.preferencesContainer, PreferencesFragment()).commit()
-
-
     }
 }
