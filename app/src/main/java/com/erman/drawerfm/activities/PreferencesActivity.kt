88ca62d89255e51c2a67a044267ab72cdb016549
Yusuf Erman ERGÖZ <ermanergoz>
2019-12-11 13:26:45 +0100

    Fixed storage button height and some more bugs

--- app/src/main/java/com/erman/drawerfm/activities/PreferencesActivity.kt
+++ app/src/main/java/com/erman/drawerfm/activities/PreferencesActivity.kt
@@ -1,6 +1,7 @@
 package com.erman.drawerfm.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.erman.drawerfm.R
 import com.erman.drawerfm.fragments.PreferencesFragment
@@ -11,8 +12,16 @@ class PreferencesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_preferences)
-
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportFragmentManager.beginTransaction()
             .replace(R.id.preferencesContainer, PreferencesFragment()).commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home ->
+                finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
