0480ac5d6e4c50f5edc6e39721a46248753b006c
Yusuf Erman ERGÖZ <ermanergoz@gmail.com>
2019-12-14 00:09:26 +0100

    Added rename and delete functionality

--- app/src/main/java/com/erman/drawerfm/utilities/FileHandler.kt
+++ app/src/main/java/com/erman/drawerfm/utilities/FileHandler.kt
@@ -10,25 +10,44 @@ fun getDirectoryData(files: List<File>): List<DirectoryData> {
 
     var fileModelList: MutableList<DirectoryData> = mutableListOf()
 
-    for (i in 0 until files.size) {
+    for (i in files.indices) {
         fileModelList.add(
             DirectoryData(
                 files[i].path,
                 files[i].isDirectory,
                 files[i].name,
-                convertFileSizeToMB(files[i].length())/*, it.extension*/,
-                files[i].listFiles()?.size
-                    ?: 0,
-                Date(files[i].lastModified())
+                convertFileSizeToMB(files[i].length()),
+                files[i].listFiles()?.size ?: 0,
+                Date(files[i].lastModified()),
+                files[i].extension
             )
         )
     }
     return fileModelList
 }
 
-
 fun convertFileSizeToMB(sizeInBytes: Long): Double {
     return (sizeInBytes.toDouble()) / (1024 * 1024)
     //TODO: Use the nice function in StorageUsageData class instead and delete this
 }
 
+fun rename(selectedDirectory: DirectoryData, newNameToBe: String) {
+    var dirName = selectedDirectory.path.removeSuffix(selectedDirectory.name)
+    var newFileName = newNameToBe
+
+    if (!selectedDirectory.isFolder) {
+        newFileName = newNameToBe + "." + selectedDirectory.extension
+    }
+    var prev = File(dirName, selectedDirectory.name)
+    var new = File(dirName, newFileName)
+
+    prev.renameTo(new)
+}
+
+fun delete(selectedDirectory: DirectoryData) {
+    if (!selectedDirectory.isFolder) {
+        File(selectedDirectory.path).deleteRecursively()
+    } else {
+        File(selectedDirectory.path).delete()
+    }
+}
