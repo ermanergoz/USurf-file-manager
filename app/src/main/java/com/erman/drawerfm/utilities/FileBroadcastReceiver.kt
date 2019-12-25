fa87fcd0bbd4656eb3cbc7fa56cdab09087826e5
Yusuf Erman ERGÖZ <erman.ergoz@hotmail.com>
2019-12-25 02:25:12 +0100

    fixed problems with refreshing

---
+++ app/src/main/java/com/erman/drawerfm/utilities/FileBroadcastReceiver.kt
@@ -0,0 +1,15 @@
+package com.erman.drawerfm.utilities
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class FileBroadcastReceiver(val path: String, private val onChange: () -> Unit) : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val filePath = intent?.extras?.getString("path for broadcast")
+        if (filePath.equals(path)) {
+            onChange.invoke()
+        }
+    }
+}
