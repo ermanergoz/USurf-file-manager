ae9de149caf1e0dec44315ba3023d0aff139ea2e
Yusuf Erman ERGÖZ <ermanergoz>
2020-01-10 01:09:45 +0100

    Finished adding toast notifications

--- app/src/main/java/com/erman/drawerfm/utilities/FileHandler.kt
+++ app/src/main/java/com/erman/drawerfm/utilities/FileHandler.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.Toast
 import com.erman.drawerfm.R
 import java.io.File
+import java.io.IOException
 
 fun getFiles(path: String): List<File> {
     return File(path).listFiles().sorted().toList()
@@ -19,20 +20,14 @@ fun getSearchedFiles(path: String, searchQuery: String): List<File> {
     return emptyList()
 }
 
-fun rename(
-    context: Context,
-    selectedDirectories: List<File>,
-    newNameToBe: String,
-    updateFragment: () -> Unit
-) {
+fun rename(context: Context, selectedDirectories: List<File>, newNameToBe: String, updateFragment: () -> Unit) {
     var isSuccess = false
 
     for (i in selectedDirectories.indices) {
         val dirName = selectedDirectories[i].path.removeSuffix(selectedDirectories[i].name)
         var newFileName = newNameToBe
 
-        if (i > 0)
-            newFileName = newFileName + "(" + i + ")"
+        if (i > 0) newFileName = newFileName + "(" + i + ")"
 
         if (!selectedDirectories[i].isDirectory) {
             newFileName = newFileName + "." + selectedDirectories[i].extension
@@ -43,20 +38,13 @@ fun rename(
         isSuccess = prev.renameTo(new)
 
         if (!isSuccess) {
-            Toast.makeText(
-                context,
-                context.getString(R.string.error_while_renaming) + prev.name,
-                Toast.LENGTH_LONG
-            ).show()
+            Toast.makeText(context, context.getString(R.string.error_while_renaming) + prev.name, Toast.LENGTH_LONG)
+                .show()
             break
         }
     }
     if (isSuccess) {
-        Toast.makeText(
-            context,
-            context.getString(R.string.renaming_successful),
-            Toast.LENGTH_LONG
-        ).show()
+        Toast.makeText(context, context.getString(R.string.renaming_successful), Toast.LENGTH_LONG).show()
 
         updateFragment.invoke()
     }
@@ -66,27 +54,21 @@ fun delete(context: Context, selectedDirectories: List<File>, updateFragment: ()
     var isSuccess = false
 
     for (i in selectedDirectories.indices) {
-        if (selectedDirectories[i].isDirectory) {
-            isSuccess = File(selectedDirectories[i].path).deleteRecursively()
+        isSuccess = if (selectedDirectories[i].isDirectory) {
+            File(selectedDirectories[i].path).deleteRecursively()
         } else {
-            isSuccess = File(selectedDirectories[i].path).delete()
+            File(selectedDirectories[i].path).delete()
         }
 
         if (!isSuccess) {
-            Toast.makeText(
-                context,
-                context.getString(R.string.error_while_renaming) + selectedDirectories[i].name,
-                Toast.LENGTH_LONG
-            ).show()
+            Toast.makeText(context,
+                           context.getString(R.string.error_while_deleting) + selectedDirectories[i].name,
+                           Toast.LENGTH_LONG).show()
             break
         }
     }
     if (isSuccess) {
-        Toast.makeText(
-            context,
-            context.getString(R.string.deleting_successful),
-            Toast.LENGTH_LONG
-        ).show()
+        Toast.makeText(context, context.getString(R.string.deleting_successful), Toast.LENGTH_LONG).show()
 
         updateFragment.invoke()
     }
@@ -96,74 +78,88 @@ fun createFolder(context: Context, path: String, folderName: String, updateFragm
     var isSuccess = File(path + "/" + folderName).mkdir()
 
     if (isSuccess) {
-        Toast.makeText(
-            context,
-            context.getString(R.string.folder_creation_successful)+folderName,
-            Toast.LENGTH_LONG
-        ).show()
+        Toast.makeText(context, context.getString(R.string.folder_creation_successful) + folderName, Toast.LENGTH_LONG)
+            .show()
 
         updateFragment.invoke()
-    }
-    else
-        Toast.makeText(
-            context,
-            context.getString(R.string.error_when_creating_folder)+folderName,
-            Toast.LENGTH_LONG
-        ).show()
+    } else Toast.makeText(context,
+                          context.getString(R.string.error_when_creating_folder) + folderName,
+                          Toast.LENGTH_LONG).show()
 }
 
 fun createFile(context: Context, path: String, folderName: String, updateFragment: () -> Unit) {
-    var isSuccess = File(path + "/" + folderName).createNewFile()
+    val isSuccess = File(path + "/" + folderName).createNewFile()
 
     if (isSuccess) {
-        Toast.makeText(
-            context,
-            context.getString(R.string.file_creation_successful)+folderName,
-            Toast.LENGTH_LONG
-        ).show()
+        Toast.makeText(context, context.getString(R.string.file_creation_successful) + folderName, Toast.LENGTH_LONG)
+            .show()
 
         updateFragment.invoke()
-    }
-    else
-        Toast.makeText(
-            context,
-            context.getString(R.string.error_when_creating_file)+folderName,
-            Toast.LENGTH_LONG
-        ).show()
+    } else Toast.makeText(context,
+                          context.getString(R.string.error_when_creating_file) + folderName,
+                          Toast.LENGTH_LONG).show()
 }
 
-fun copyFile(
-    copyOrMoveSources: List<File>,
-    copyOrMoveDestination: String,
-    updateFragment: () -> Unit
-) {
+fun copyFile(context: Context,
+             copyOrMoveSources: List<File>,
+             copyOrMoveDestination: String,
+             updateFragment: () -> Unit) {
+    var isSuccess = false
+
     for (i in copyOrMoveSources.indices) {
         if (copyOrMoveSources[i].isDirectory) {
-            File(copyOrMoveSources[i].path).copyRecursively(File(copyOrMoveDestination + copyOrMoveSources[i].name))
+            isSuccess =
+                File(copyOrMoveSources[i].path).copyRecursively(File(copyOrMoveDestination + copyOrMoveSources[i].name))
         } else {
-            File(copyOrMoveSources[i].path).copyTo(File(copyOrMoveDestination + "/" + copyOrMoveSources[i].name))
+            try {
+                File(copyOrMoveSources[i].path).copyTo(File(copyOrMoveDestination + "/" + copyOrMoveSources[i].name))
+            } catch (err: IOException) {
+                isSuccess = false
+                break
+            }
+            isSuccess = true
         }
     }
-    updateFragment.invoke()
+
+    if (isSuccess) {
+        Toast.makeText(context, context.getString(R.string.copy_successful), Toast.LENGTH_LONG).show()
+        updateFragment.invoke()
+    } else {
+        Toast.makeText(context, context.getString(R.string.error_while_copying), Toast.LENGTH_LONG).show()
+    }
 }
 
-fun moveFile(
-    copyOrMoveSources: List<File>,
-    copyOrMoveDestination: String,
-    updateFragment: () -> Unit
-) {
+fun moveFile(context: Context,
+             copyOrMoveSources: List<File>,
+             copyOrMoveDestination: String,
+             updateFragment: () -> Unit) {
+    var isSuccess = false
+
     for (i in copyOrMoveSources.indices) {
         if (copyOrMoveSources[i].isDirectory) {
-            File(copyOrMoveSources[i].path).copyRecursively(File(copyOrMoveDestination + copyOrMoveSources[i].name))
+            isSuccess =
+                File(copyOrMoveSources[i].path).copyRecursively(File(copyOrMoveDestination + copyOrMoveSources[i].name))
         } else {
-            File(copyOrMoveSources[i].path).copyTo(File(copyOrMoveDestination + "/" + copyOrMoveSources[i].name))
+            try {
+                File(copyOrMoveSources[i].path).copyTo(File(copyOrMoveDestination + "/" + copyOrMoveSources[i].name))
+            } catch (err: IOException) {
+                isSuccess = false
+                break
+            }
+            isSuccess = true
         }
 
-        if (copyOrMoveSources[i].isDirectory) {
-            File(copyOrMoveSources[i].path).deleteRecursively()
-        } else {
-            File(copyOrMoveSources[i].path).delete()
+        if (copyOrMoveSources[i].isDirectory && isSuccess) {
+            isSuccess = File(copyOrMoveSources[i].path).deleteRecursively()
+        } else if (!copyOrMoveSources[i].isDirectory && isSuccess) {
+            isSuccess = File(copyOrMoveSources[i].path).delete()
         }
     }
-    updateFragment.invoke()
+
+    if (isSuccess) {
+        Toast.makeText(context, context.getString(R.string.moving_successful), Toast.LENGTH_LONG).show()
+        updateFragment.invoke()
+    } else {
+        Toast.makeText(context, context.getString(R.string.error_while_moving), Toast.LENGTH_LONG).show()
+    }
 }
