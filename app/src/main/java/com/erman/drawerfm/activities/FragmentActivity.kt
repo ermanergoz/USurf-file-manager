ad39873b9c176f59ea4fe9508c591ca9cc152056
Erman <ermanergoz@gmail.com>
2020-08-05 20:36:35 +0200

    deneme

--- app/src/main/java/com/erman/drawerfm/activities/FragmentActivity.kt
+++ app/src/main/java/com/erman/drawerfm/activities/FragmentActivity.kt
@@ -33,6 +33,7 @@ import com.erman.drawerfm.common.*
 class FragmentActivity : AppCompatActivity(), OnFileClickListener, RenameDialog.DialogRenameFileListener, CreateNew.DialogCreateFolderListener,
     SearchView.OnQueryTextListener {
     private var newShortcutPath = ""
+
     private var isCreateShortcutMode = false
     lateinit var path: String
     private lateinit var longClickedFile: File
