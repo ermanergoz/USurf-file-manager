5878e86984508dc6d0983e31bae6a0a0e75ecf11
Yusuf Erman ERGÖZ <erman.ergoz@hotmail.com>
2020-09-02 00:32:43 +0200

    implement directory listing and browsing

--- app/src/main/java/com/erman/usurf/directory/model/DirectoryModel.kt
+++ app/src/main/java/com/erman/usurf/directory/model/DirectoryModel.kt
@@ -1,5 +1,140 @@
 package com.erman.usurf.directory.model
 
+import java.io.File
+import java.lang.Exception
+
 class DirectoryModel() {
 
+    fun getFiles(
+        path: String/*,
+        showHidden: Boolean,
+        showFilesOnly: Boolean,
+        showFoldersOnly: Boolean,
+        fileSortMode: String?,
+        isAscending: Boolean,
+        isDescending: Boolean,
+        showFilesOnTop: Boolean,
+        showFoldersOnTop: Boolean*/
+    ): List<File> {
+        var files = listOf<File>()
+        try {
+            files = File(path).listFiles()!!.toList()
+        } catch (err: Exception) {
+            err.printStackTrace()
+        }
+
+
+        //var files = File(path).listFiles()!!.filter { !it.isHidden || showHidden }
+        //    .filter { it.isFile || !showFilesOnly }
+        //    .filter { it.isDirectory || !showFoldersOnly }.toMutableList()
+//
+        //if (fileSortMode == "Sort by name") {
+        //    if (isAscending) files =
+        //        files.sortedWith(
+        //            compareBy(
+        //                { !it.isDirectory || !showFoldersOnTop },
+        //                { !it.isFile || !showFilesOnTop },
+        //                { it.name })
+        //        ).toMutableList()
+        //    else if (isDescending) {
+        //        files = files.sortedWith(compareBy<File>({ !it.isDirectory || !showFoldersOnTop },
+        //            { !it.isFile || !showFilesOnTop }).thenByDescending { it.name }).toMutableList()
+        //    }
+        //}
+//
+        //if (fileSortMode == "Sort by size") {
+        //    if (isAscending) files = files.sortedWith(
+        //        compareBy({ !it.isDirectory || !showFoldersOnTop },
+        //            { !it.isFile || !showFilesOnTop },
+        //            { it.length() },
+        //            {
+        //                getFolderSize(
+        //                    it.path
+        //                )
+        //            })
+        //    ).toMutableList()
+        //    else if (isDescending) {
+        //        files = files.sortedWith(
+        //            compareBy({ !it.isDirectory || !showFoldersOnTop },
+        //                { !it.isFile || !showFilesOnTop },
+        //                { it.length() },
+        //                {
+        //                    getFolderSize(
+        //                        it.path
+        //                    )
+        //                })
+        //        ).asReversed().toMutableList()
+        //        /*asReversed() is a view of the sorted list with reversed index and has better performance than using reverse()*/
+        //        /*in this case we have 2 descending fields and compareByDescending can take only one*/
+        //    }
+        //}
+        //if (fileSortMode == "Sort by last modified") {
+        //    if (isAscending) files =
+        //        files.sortedWith(
+        //            compareBy(
+        //                { !it.isDirectory || !showFoldersOnTop },
+        //                { !it.isFile || !showFilesOnTop },
+        //                { it.lastModified() })
+        //        )
+        //            .toMutableList()
+        //    else if (isDescending) {
+        //        files = files.sortedWith(compareBy<File>({ !it.isDirectory || !showFoldersOnTop },
+        //            { !it.isFile || !showFilesOnTop }).thenByDescending { it.lastModified() })
+        //            .toMutableList()
+        //    }
+        //}
+        return files
+    }
+
+    fun getFolderSize(path: String): Double {
+        if (File(path).exists()) {
+            var size = 0.0
+
+            var fileList = File(path).listFiles().toList()
+
+            for (i in fileList.indices) {
+                if (fileList[i].isDirectory) {
+                    size += getFolderSize(fileList[i].path)
+                } else size += fileList[i].length()
+            }
+            return size
+        }
+        return 0.0
+    }
+
+    fun getConvertedFileSize(size: Long): String {
+        var sizeStr = ""
+
+        val kilobyte = size / 1024.0
+        val megabyte = size / (1024.0 * 1024.0)
+        val gigabyte = size / (1024.0 * 1024.0 * 1024.0)
+        val terabyte = size / (1024.0 * 1024.0 * 1024.0 * 1024.0)
+
+        sizeStr = when {
+            terabyte > 1 -> "%.2f".format(terabyte) + " TB"
+            gigabyte > 1 -> "%.2f".format(gigabyte) + " GB"
+            megabyte > 1 -> "%.2f".format(megabyte) + " MB"
+            kilobyte > 1 -> "%.2f".format(kilobyte) + " KB"
+            else -> size.toDouble().toString() + " Bytes"
+        }
+        return sizeStr
+    }
+
+    fun getConvertedFileSize(size: Double): String {
+        var sizeStr = ""
+
+        val kilobyte = size / 1024.0
+        val megabyte = size / (1024.0 * 1024.0)
+        val gigabyte = size / (1024.0 * 1024.0 * 1024.0)
+        val terabyte = size / (1024.0 * 1024.0 * 1024.0 * 1024.0)
+
+        sizeStr = when {
+            terabyte > 1 -> "%.2f".format(terabyte) + " TB"
+            gigabyte > 1 -> "%.2f".format(gigabyte) + " GB"
+            megabyte > 1 -> "%.2f".format(megabyte) + " MB"
+            kilobyte > 1 -> "%.2f".format(kilobyte) + " KB"
+            else -> size.toDouble().toString() + " Bytes"
+        }
+        return sizeStr
+    }
 }
--- app/src/main/java/com/erman/usurf/home/model/HomeModel.kt
+++ app/src/main/java/com/erman/usurf/home/model/HomeModel.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.Build
 import android.os.Environment
 import android.os.StatFs
-import android.util.Log
 import android.view.LayoutInflater
 import androidx.databinding.DataBindingUtil
 import com.erman.usurf.MainApplication.Companion.appContext
@@ -143,6 +142,7 @@ class HomeModel() {
             storageButtons.add(binding)
             storageButtons[i].root.tag = storageDirectories.elementAt(i)
             storageButtons[i].root.buttonText.text=storageDirectories.elementAt(i)
+            storageButtons[i].root.progressBar.scaleY=20f
         }
         return storageButtons
     }
@@ -167,80 +167,25 @@ class HomeModel() {
     }
 
     fun getUsedStoragePercentage(path: String): Int {
-        Log.e("usd path", path)
-
-        Log.e("usd path perc", ((getUsedStorage(
-            path
-        ) * 100 / getTotalStorage(path))).toInt().toString())
         if (path != "/" && (getTotalStorage(path)).toInt() != 0) return ((getUsedStorage(
             path
         ) * 100 / getTotalStorage(path))).toInt()
         return 0
     }
 
-    fun getFolderUsedStoragePercentage(path: String): Double {
-        if (path != "/" && (getTotalStorage(path)).toInt() != 0) return ((getFolderSize(
-            path
-        ) * 100.0 / getTotalStorage(path)))
-        return 0.0
-    }
+    //fun getFolderUsedStoragePercentage(path: String): Double {
+    //    if (path != "/" && (getTotalStorage(path)).toInt() != 0) return ((getFolderSize(
+    //        path
+    //    ) * 100.0 / getTotalStorage(path)))
+    //    return 0.0
+    //}
 
-    fun getFolderUsedStoragePercentage(path: String, size: Long): Double {
-        if (path != "/" && (getTotalStorage(path)).toDouble() != 0.0) return ((size * 100.0 / getTotalStorage(
-            path
-        )))
-        return 0.0
-    }
+    //fun getFolderUsedStoragePercentage(path: String, size: Long): Double {
+    //    if (path != "/" && (getTotalStorage(path)).toDouble() != 0.0) return ((size * 100.0 / getTotalStorage(
+    //        path
+    //    )))
+    //    return 0.0
+    //}
 
-    fun getFolderSize(path: String): Double {
-        if (File(path).exists()) {
-            var size = 0.0
 
-            var fileList = File(path).listFiles().toList()
-
-            for (i in fileList.indices) {
-                if (fileList[i].isDirectory) {
-                    size += getFolderSize(fileList[i].path)
-                } else size += fileList[i].length()
-            }
-            return size
-        }
-        return 0.0
-    }
-
-    fun getConvertedFileSize(size: Long): String {
-        var sizeStr = ""
-
-        val kilobyte = size / 1024.0
-        val megabyte = size / (1024.0 * 1024.0)
-        val gigabyte = size / (1024.0 * 1024.0 * 1024.0)
-        val terabyte = size / (1024.0 * 1024.0 * 1024.0 * 1024.0)
-
-        sizeStr = when {
-            terabyte > 1 -> "%.2f".format(terabyte) + " TB"
-            gigabyte > 1 -> "%.2f".format(gigabyte) + " GB"
-            megabyte > 1 -> "%.2f".format(megabyte) + " MB"
-            kilobyte > 1 -> "%.2f".format(kilobyte) + " KB"
-            else -> size.toDouble().toString() + " Bytes"
-        }
-        return sizeStr
-    }
-
-    fun getConvertedFileSize(size: Double): String {
-        var sizeStr = ""
-
-        val kilobyte = size / 1024.0
-        val megabyte = size / (1024.0 * 1024.0)
-        val gigabyte = size / (1024.0 * 1024.0 * 1024.0)
-        val terabyte = size / (1024.0 * 1024.0 * 1024.0 * 1024.0)
-
-        sizeStr = when {
-            terabyte > 1 -> "%.2f".format(terabyte) + " TB"
-            gigabyte > 1 -> "%.2f".format(gigabyte) + " GB"
-            megabyte > 1 -> "%.2f".format(megabyte) + " MB"
-            kilobyte > 1 -> "%.2f".format(kilobyte) + " KB"
-            else -> size.toDouble().toString() + " Bytes"
-        }
-        return sizeStr
-    }
 }
