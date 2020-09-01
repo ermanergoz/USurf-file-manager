2326ad735491c7dd07161a28ce28969e4db84b5f
Yusuf Erman ERGÖZ <ermanergoz@gmail.com>
2020-09-02 00:32:43 +0200

    Implement directory listing and browsing

--- app/src/main/java/com/erman/usurf/directory/ui/DirectoryViewModel.kt
+++ app/src/main/java/com/erman/usurf/directory/ui/DirectoryViewModel.kt
@@ -1,14 +1,64 @@
 package com.erman.usurf.directory.ui
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.erman.usurf.directory.model.DirectoryModel
+import com.erman.usurf.directory.utils.SIMPLE_DATE_FORMAT_PATTERN
+import java.io.File
+import java.lang.Exception
+import java.text.SimpleDateFormat
 
 class DirectoryViewModel(private val directoryModel: DirectoryModel) : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is directory Fragment"
+    private val _path = MutableLiveData<String>().apply {
+        value = ""
     }
-    val text: LiveData<String> = _text
+    val path: MutableLiveData<String> = _path
+
+    private val dateFormat = SimpleDateFormat(SIMPLE_DATE_FORMAT_PATTERN)
+    fun getLastModified(file: File): String {
+        return dateFormat.format(file.lastModified())
+    }
+
+    fun getFileExtension(file: File): String {
+        return if (file.isFile) file.extension
+        else ""
+    }
+
+    fun getFileSize(file: File): String {
+        return directoryModel.getConvertedFileSize(file.length())
+    }
+
+    fun getSubFileCount(file: File): String {
+        return try {
+            val fileCount = File(file.path).listFiles()!!.size
+            if (fileCount == 0) "Empty Folder"
+            else "$fileCount files"
+        } catch (err: Exception) {
+            err.printStackTrace()
+            ""
+        }
+    }
+
+    fun getFileList(): List<File> {
+        return if (!path.value.isNullOrEmpty()) {
+            directoryModel.getFiles(path.value!!)
+        } else emptyList()
+    }
+
+    fun onFileClick(file: File) {
+        path.value = file.path
+    }
+
+    fun onBackPressed(): Boolean {
+        try {
+            val prevFile = File(File(path.value!!).parent!!)
+            if (prevFile.canRead()) {
+                path.value = prevFile.path
+                return true
+            }
+        } catch (err: Exception) {
+            err.printStackTrace()
+        }
+        return false
+    }
 }
--- app/src/main/java/com/erman/usurf/home/ui/HomeFragment.kt
+++ app/src/main/java/com/erman/usurf/home/ui/HomeFragment.kt
@@ -9,15 +9,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.erman.usurf.R
 import com.erman.usurf.databinding.FragmentHomeBinding
+import com.erman.usurf.utils.EventObserver
+import com.erman.usurf.directory.ui.DirectoryViewModel
 import com.erman.usurf.utils.ViewModelFactory
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
-
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var directoryViewModel: DirectoryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +32,17 @@ class HomeFragment : Fragment() {
         val binding: FragmentHomeBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
+        directoryViewModel =
+            ViewModelProvider(requireActivity(), viewModelFactory).get(DirectoryViewModel::class.java)
+
+        homeViewModel.openTaskEvent.observe(viewLifecycleOwner, EventObserver {
+            findNavController().navigate(it)
+        })
+
+        homeViewModel.storagePath.observe(viewLifecycleOwner, Observer {
+            directoryViewModel.path.value = it
+        })
+
         binding.lifecycleOwner = this
         binding.viewModel = homeViewModel
         return binding.root
@@ -36,7 +50,6 @@ class HomeFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-
         //To avoid storage buttons from disappearing when resuming the app from background
         homeViewModel.storageButtons.observe(viewLifecycleOwner, Observer {
             val buttonLayoutParams =
