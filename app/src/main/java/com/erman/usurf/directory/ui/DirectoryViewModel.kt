0ee1fe9c401f9973558e3093559b8c79a795ab82
Yusuf Erman ERGÖZ <erman.ergoz@hotmail.com>
2020-09-03 17:21:27 +0200

    Add functionality to open the file

--- app/src/main/java/com/erman/usurf/directory/ui/DirectoryViewModel.kt
+++ app/src/main/java/com/erman/usurf/directory/ui/DirectoryViewModel.kt
@@ -1,9 +1,13 @@
 package com.erman.usurf.directory.ui
 
+import android.content.ActivityNotFoundException
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.erman.usurf.R
 import com.erman.usurf.directory.model.DirectoryModel
 import com.erman.usurf.directory.utils.SIMPLE_DATE_FORMAT_PATTERN
+import com.erman.usurf.utils.Event
 import java.io.File
 import java.lang.Exception
 import java.text.SimpleDateFormat
@@ -12,7 +16,10 @@ class DirectoryViewModel(private val directoryModel: DirectoryModel) : ViewModel
     private val _path = MutableLiveData<String>().apply {
         value = ""
     }
-    val path: MutableLiveData<String> = _path
+
+    //TODO: It is considered a good practice to expose a LiveData instance and keep the actual
+    // MutableLiveData private. Change other viewmodels accordingly!
+    val path: MutableLiveData/*change to LiveData*/<String> = _path
 
     private val dateFormat = SimpleDateFormat(SIMPLE_DATE_FORMAT_PATTERN)
     fun getLastModified(file: File): String {
@@ -45,16 +52,52 @@ class DirectoryViewModel(private val directoryModel: DirectoryModel) : ViewModel
         } else emptyList()
     }
 
+    private val _toastMessage = MutableLiveData<Event<Int>>()
+    val toastMessage: LiveData<Event<Int>> = _toastMessage
+
     fun onFileClick(file: File) {
-        path.value = file.path
+        if (file.isDirectory)
+            _path.value = file.path
+        else {
+            try {
+                directoryModel.openFile(file)
+            } catch (err: ActivityNotFoundException) {
+                _toastMessage.value = Event(R.string.file_unsupported_or_no_application)
+            }
+        }
+    }
+
+    private val _optionMode = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    val optionMode: LiveData<Boolean> = _optionMode
+
+    fun onFileLongClick(file: File): Boolean {
+        _optionMode.value = true
+        return true
+    }
+
+    private val _moreOptionMode = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    val moreOptionMode: LiveData<Boolean> = _moreOptionMode
+
+    fun onMoreClicked() {
+        _moreOptionMode.value = !_moreOptionMode.value!!
     }
 
     fun onBackPressed(): Boolean {
         try {
-            val prevFile = File(File(path.value!!).parent!!)
-            if (prevFile.canRead()) {
-                path.value = prevFile.path
+            if (optionMode.value!!) {
+                _optionMode.value = false
+                _moreOptionMode.value = false
                 return true
+            } else {
+                val prevFile = File(File(path.value!!).parent!!)
+                if (prevFile.canRead()) {
+                    _path.value = prevFile.path
+                    return true
+                }
             }
         } catch (err: Exception) {
             err.printStackTrace()
