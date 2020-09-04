02d5a5e965cf902b83fdf66124a189b71489187a
Yusuf Erman ERGÖZ <ermanergoz@gmail.com>
2020-09-04 02:22:52 +0200

    Implement recyclerview multiple selection

--- app/src/main/java/com/erman/usurf/directory/ui/DirectoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/erman/usurf/directory/ui/DirectoryRecyclerViewAdapter.kt
@@ -1,23 +1,21 @@
 package com.erman.usurf.directory.ui
 
-import android.content.Context
+import android.graphics.Color
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.erman.usurf.R
-import com.erman.usurf.directory.utils.MARQUEE_CHOICE_KEY
 import com.erman.usurf.directory.utils.MARQUEE_REPEAT_LIM
-import com.erman.usurf.utils.SHARED_PREF_FILE
 import kotlinx.android.synthetic.main.recycler_directory_layout.view.*
-import java.io.File
 import androidx.databinding.library.baseAdapters.BR
 import com.erman.usurf.databinding.RecyclerDirectoryLayoutBinding
+import com.erman.usurf.utils.FileModel
 
 class DirectoryRecyclerViewAdapter(var viewModel: DirectoryViewModel) :
     RecyclerView.Adapter<DirectoryRecyclerViewAdapter.ViewHolder>() {
-    var directoryList = listOf<File>()
+    var directoryList = listOf<FileModel>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding: RecyclerDirectoryLayoutBinding =
@@ -41,23 +39,29 @@ class DirectoryRecyclerViewAdapter(var viewModel: DirectoryViewModel) :
 
     inner class ViewHolder(var binding: RecyclerDirectoryLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bindDirectory(directory: File) {
+        fun bindDirectory(directory: FileModel) {
             binding.setVariable(BR.file, directory)
-            if (itemView.context.getSharedPreferences(
-                    SHARED_PREF_FILE,
-                    Context.MODE_PRIVATE
-                ).getBoolean(MARQUEE_CHOICE_KEY, true)
-            ) {
-                itemView.nameTextView.ellipsize =
-                    TextUtils.TruncateAt.MARQUEE  //for sliding names if the length is longer than 1 line
-                itemView.nameTextView.isSelected = true
-                itemView.nameTextView.marqueeRepeatLimit = MARQUEE_REPEAT_LIM   //-1 is for forever
+
+            if (directory.isSelected) {
+                itemView.setBackgroundColor(Color.parseColor("#99CBFD"))
+            } else {
+                itemView.setBackgroundColor(Color.TRANSPARENT)
             }
+
+            itemView.nameTextView.ellipsize =
+                TextUtils.TruncateAt.MARQUEE  //for sliding names if the length is longer than 1 line
+            itemView.nameTextView.isSelected = true
+            itemView.nameTextView.marqueeRepeatLimit = MARQUEE_REPEAT_LIM   //-1 is for forever
         }
     }
 
-    fun updateData(filesList: List<File>) {
+    fun updateData(filesList: List<FileModel>) {
         this.directoryList = filesList
+        notifyDataSetChanged()
+    }
+
+    fun updateSelection(selectedFileList: MutableList<FileModel>) {
+        //TODO: Improve here to fix screen flickering
         notifyDataSetChanged()
     }
 }
