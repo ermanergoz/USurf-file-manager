0059d3509955085ac74f8fd1fff5944c9838cbcd
Yusuf Erman ERGOZ <ermanergoz@gmail.com>
2020-09-04 21:50:30 +0200

    Cleanup and refactor

--- app/src/main/java/com/erman/usurf/directory/ui/DirectoryViewModel.kt
+++ app/src/main/java/com/erman/usurf/directory/ui/DirectoryViewModel.kt
@@ -1,40 +1,43 @@
 package com.erman.usurf.directory.ui
 
 import android.content.ActivityNotFoundException
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.erman.usurf.R
 import com.erman.usurf.directory.model.DirectoryModel
-import com.erman.usurf.directory.utils.SIMPLE_DATE_FORMAT_PATTERN
 import com.erman.usurf.utils.Event
-import com.erman.usurf.utils.FileModel
+import com.erman.usurf.directory.model.FileModel
 import java.io.File
 import java.lang.Exception
-import java.text.SimpleDateFormat
 
 class DirectoryViewModel(private val directoryModel: DirectoryModel) : ViewModel() {
-    private val _path = MutableLiveData<String>().apply {
-        value = ""
-    }
+    private var multiSelectionMode: Boolean = false
 
-    //TODO: It is considered a good practice to expose a LiveData instance and keep the actual
-    // MutableLiveData private. Change other viewmodels accordingly!
-    val path: MutableLiveData/*change to LiveData*/<String> = _path
-
-    fun getFileList(): List<FileModel> {
-        return if (!path.value.isNullOrEmpty()) {
-            directoryModel.getFileModelsFromFiles(path.value!!)
-        } else emptyList()
-    }
+    private val _path = MutableLiveData<String>()
+    val path: LiveData<String> = _path
 
     private val _toastMessage = MutableLiveData<Event<Int>>()
     val toastMessage: LiveData<Event<Int>> = _toastMessage
 
+    private val _optionMode = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    val optionMode: LiveData<Boolean> = _optionMode
+
+    private val _multipleSelection = MutableLiveData<MutableList<FileModel>>().apply {
+        value = mutableListOf()
+    }
+    val multipleSelection: LiveData<MutableList<FileModel>> = _multipleSelection
+
+    private val _moreOptionMode = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    val moreOptionMode: LiveData<Boolean> = _moreOptionMode
+
     fun onFileClick(file: FileModel) {
-        if (multiSelectionMode.value!!)
-            manageMultipleSelectionList(file)
+        if (multiSelectionMode)
+            _multipleSelection.value = directoryModel.manageMultipleSelectionList(file, multipleSelection.value!!)
         else {
             if (file.isDirectory)
                 _path.value = file.path
@@ -48,44 +51,12 @@ class DirectoryViewModel(private val directoryModel: DirectoryModel) : ViewModel
         }
     }
 
-    private val _optionMode = MutableLiveData<Boolean>().apply {
-        value = false
-    }
-    val optionMode: LiveData<Boolean> = _optionMode
-
-    private val _multiSelectionMode = MutableLiveData<Boolean>().apply {
-        value = false
-    }
-    val multiSelectionMode: LiveData<Boolean> = _multiSelectionMode
-
-    private val _multipleSelection = MutableLiveData<MutableList<FileModel>>().apply {
-        value = mutableListOf()
-    }
-    val multipleSelection: LiveData<MutableList<FileModel>> = _multipleSelection
-
     fun onFileLongClick(file: FileModel): Boolean {
-        manageMultipleSelectionList(file)
+        _multipleSelection.value = directoryModel.manageMultipleSelectionList(file, multipleSelection.value!!)
         _optionMode.value = true
-        _multiSelectionMode.value = true
+        multiSelectionMode = true
         return true
     }
-
-    fun manageMultipleSelectionList(file: FileModel) {
-        val selection = multipleSelection.value!!
-        if (file.isSelected) {
-            file.isSelected = false
-            selection.remove(file)
-        } else {
-            file.isSelected = true
-            selection.add(file)
-        }
-        _multipleSelection.value = selection
-    }
-
-    private val _moreOptionMode = MutableLiveData<Boolean>().apply {
-        value = false
-    }
-    val moreOptionMode: LiveData<Boolean> = _moreOptionMode
 
     fun onMoreClicked() {
         _moreOptionMode.value = !_moreOptionMode.value!!
@@ -94,9 +65,10 @@ class DirectoryViewModel(private val directoryModel: DirectoryModel) : ViewModel
     fun onBackPressed(): Boolean {
         try {
             if (optionMode.value!!) {
+                _multipleSelection.value = directoryModel.clearMultipleSelection(multipleSelection.value!!)
                 _optionMode.value = false
                 _moreOptionMode.value = false
-                _multiSelectionMode.value = false
+                multiSelectionMode = false
                 return true
             } else {
                 val prevFile = File(File(path.value!!).parent!!)
@@ -109,5 +81,15 @@ class DirectoryViewModel(private val directoryModel: DirectoryModel) : ViewModel
             err.printStackTrace()
         }
         return false
+    }
+
+    fun setPath(path: String) {
+        _path.value = path
+    }
+
+    fun getFileList(): List<FileModel> {
+        return if (!path.value.isNullOrEmpty()) {
+            directoryModel.getFileModelsFromFiles(path.value!!)
+        } else emptyList()
     }
 }
