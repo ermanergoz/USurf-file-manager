86a4054c6886a1e4834b2025e4051a6db2d13bc7
Yusuf Erman ERGOZ <erman.ergoz@gmail.com>
2020-10-15 17:01:11 +0200

    Finish implementing storage buttons

--- app/src/main/java/com/erman/usurf/app/data/ApplicationDao.kt
+++ app/src/main/java/com/erman/usurf/app/data/ApplicationDao.kt
@@ -3,9 +3,9 @@ package com.erman.usurf.app.data
 import com.erman.usurf.app.utils.INITIAL_SHORTCUT_LIST
 import com.erman.usurf.home.data.Shortcut
 import com.erman.usurf.utils.StoragePaths
+import com.erman.usurf.utils.loge
 import io.realm.Realm
 import io.realm.kotlin.createObject
-import io.realm.kotlin.where
 import java.io.File
 
 class ApplicationDao(val realm: Realm) {
@@ -17,10 +17,15 @@ class ApplicationDao(val realm: Realm) {
 
             if (File(shortcutPath).exists()) {
                 realm.beginTransaction()
-                val shortcut: Shortcut = realm.createObject<Shortcut>((realm.where<Shortcut>().findAll().size) + 1)
-                shortcut.name = shortcutName
-                shortcut.path = shortcutPath
-                realm.commitTransaction()
+                try {
+                    val shortcut: Shortcut = realm.createObject<Shortcut>(shortcutPath)
+                    shortcut.name = shortcutName
+                    shortcut.path = shortcutPath
+                } catch (err: Error) {
+                    loge("addInitialShortcuts $err")
+                } finally {
+                    realm.commitTransaction()
+                }
             }
         }
     }
