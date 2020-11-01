15ce5702ec166003358764c628f733e204c764ff
Yusuf Erman ERGOZ <erman.ergoz@gmail.com>
2020-11-02 00:51:34 +0300

    Fix ftp server bugs

--- app/src/main/java/com/erman/usurf/ftp/model/FtpServer.kt
+++ app/src/main/java/com/erman/usurf/ftp/model/FtpServer.kt
@@ -1,9 +1,6 @@
 package com.erman.usurf.ftp.model
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -13,10 +10,12 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.erman.usurf.activity.MainActivity
 import com.erman.usurf.R
+import com.erman.usurf.application.MainApplication
 import com.erman.usurf.ftp.data.FtpPreferenceProvider
 import com.erman.usurf.ftp.utils.*
 import com.erman.usurf.utils.SHARED_PREF_FILE
 import com.erman.usurf.utils.logd
+import com.erman.usurf.utils.loge
 import org.apache.ftpserver.ConnectionConfigFactory
 import org.apache.ftpserver.FtpServer
 import org.apache.ftpserver.FtpServerFactory
@@ -58,12 +57,22 @@ class FtpServer : Service() {
         serverFactory.userManager.save(user)
         serverFactory.addListener("default", listenerFactory.createListener())
         server?.let {
-            it.start()
-            sendBroadcast()
-            isFtpServerRunning = true
-            logd("FTP server started")
+            try {
+                it.start()
+                sendBroadcast()
+                isFtpServerRunning = true
+                logd("FTP server started")
+            } catch (err: Exception) {
+                loge("onStartCommand $err")
+
+                it.stop()
+                sendBroadcast()
+                isFtpServerRunning = false
+                removeNotification()
+                logd("FTP server stopped")
+            }
         }
-        displayNotification()
+        //displayNotification()
         return START_STICKY //will restart if the android system terminates for any reason.
     }
 
@@ -86,34 +95,33 @@ class FtpServer : Service() {
         LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(broadcastIntent)
     }
 
-    private fun displayNotification() {
+    private fun getNotification(): Notification {
         createNotificationChannel()
 
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra(KEY_INTENT_IS_FTP_NOTIFICATION_CLICKED, true)
 
         val pendingIntent =
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            PendingIntent.getActivity(this, PENDING_INTENT_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val builder =
-            NotificationCompat.Builder(this, CHANNEL_ID).setSmallIcon(R.drawable.ic_ftp_notification)
+            NotificationCompat.Builder(this, FTP_NOTIFICATION_CHANNEL_ID).setSmallIcon(R.drawable.ic_notification)
                 .setContentTitle(getString(R.string.ftp_server_is_running))
                 .setContentIntent(pendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT).setOngoing(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setOngoing(true)
 
-        with(NotificationManagerCompat.from(this)) {
-            notify(1, builder.build())
-        }
+        return builder.build()
     }
 
     private fun createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.notification_channel_name)
-            val descriptionText = getString(R.string.notification_channel_description)
+            val name = getString(R.string.ftp_notification_channel_name)
+            val descriptionText = getString(R.string.ftp_notification_channel_description)
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            val channel = NotificationChannel(FTP_NOTIFICATION_CHANNEL_ID, name, importance).apply {
                 description = descriptionText
             }
             // Register the channel with the system
@@ -123,6 +131,36 @@ class FtpServer : Service() {
     }
 
     private fun removeNotification() {
-        NotificationManagerCompat.from(this).cancel(1)
+        NotificationManagerCompat.from(this).cancel(FTP_NOTIFICATION_ID)
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+
+        Intent(MainApplication.appContext, FtpServer()::class.java).also { intent ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForeground(FTP_NOTIFICATION_ID , getNotification())
+            } else {
+                startService(intent)
+                with(NotificationManagerCompat.from(this)) {
+                    notify(FTP_NOTIFICATION_ID, getNotification())
+                }
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Intent(MainApplication.appContext, FtpServer()::class.java).also { intent ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForeground(FTP_NOTIFICATION_ID , getNotification())
+            } else {
+                startService(intent)
+                with(NotificationManagerCompat.from(this)) {
+                    notify(FTP_NOTIFICATION_ID, getNotification())
+                }
+            }
+        }
     }
 }
