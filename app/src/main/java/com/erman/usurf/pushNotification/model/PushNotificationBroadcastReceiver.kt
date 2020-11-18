eb5576aca5a68c8a092b1066ffb61e8ade84ed51
Yusuf Erman ERGOZ <erman.ergoz@gmail.com>
2020-11-18 21:19:47 +0300

    various bug fixes

--- app/src/main/java/com/erman/usurf/pushNotification/model/PushNotificationBroadcastReceiver.kt
+++ app/src/main/java/com/erman/usurf/pushNotification/model/PushNotificationBroadcastReceiver.kt
@@ -3,9 +3,14 @@ package com.erman.usurf.pushNotification.model
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.erman.usurf.utils.loge
 
 class PushNotificationBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
-        context.startService(Intent(context, PushNotificationIntentService::class.java))
+        try {
+            context.startService(Intent(context, PushNotificationIntentService::class.java))
+        } catch (err: Exception) {
+            loge("onReceive $err")
+        }
     }
 }
