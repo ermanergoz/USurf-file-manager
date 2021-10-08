f03343f5d63a9cdd27eb1a812e078804c01ab423
Yusuf Erman ERGOZ <erman.ergoz@gmail.com>
2021-10-09 00:13:01 +0300

    Clean up

--- app/src/main/java/com/erman/usurf/activity/MainActivity.kt
+++ app/src/main/java/com/erman/usurf/activity/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
