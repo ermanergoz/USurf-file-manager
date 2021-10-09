e433d531577b2d488b1c82362a68eb5b8a89b3ae
Yusuf Erman ERGÖZ <ermanergoz@gmail.com>
2021-10-10 02:02:29 +0300

    Refactor image loading with RGB565 format for lower memory usage

--- app/src/main/java/com/erman/usurf/directory/ui/DirectoryRecyclerViewBindingAdapter.kt
+++ app/src/main/java/com/erman/usurf/directory/ui/DirectoryRecyclerViewBindingAdapter.kt
@@ -1,22 +1,26 @@
 package com.erman.usurf.directory.ui
 
-import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.erman.usurf.R
 import com.erman.usurf.directory.utils.THUMBNAIL_IMAGE_HEIGHT
 import com.erman.usurf.directory.utils.THUMBNAIL_IMAGE_WIDTH
 import com.erman.usurf.directory.utils.THUMBNAIL_SIZE_MULTIPLIER
+import java.io.File
 
-@BindingAdapter("filePath", "isDirectory", "placeHolder", requireAll = false)
-fun setImage(imageView: ImageView, filePath: String, isDirectory: Boolean, placeHolder: Drawable) {
-    if (!isDirectory) {
+@BindingAdapter("filePath")
+fun setImage(imageView: ImageView, filePath: String) {
+    val file = File(filePath)
+    if (file.isFile) {
         val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE)
+            .format(DecodeFormat.PREFER_RGB_565) //uses less memory than default ARGB_8888
             .override(THUMBNAIL_IMAGE_WIDTH, THUMBNAIL_IMAGE_HEIGHT)
-            .placeholder(placeHolder)
+            .placeholder(R.drawable.ic_file)
 
         Glide.with(imageView.context).load(filePath).thumbnail(THUMBNAIL_SIZE_MULTIPLIER).apply(requestOptions).into(imageView)
-    }
+    } else imageView.setImageResource(R.drawable.ic_folder)
 }
