4685f0688ab2d04591c3f9374c3b82871db6c3e8
Yusuf Erman ERGÖZ <ermanergoz@gmail.com>
2021-10-10 14:35:24 +0300

    Add thumbnail preference

--- app/src/main/java/com/erman/usurf/directory/ui/DirectoryRecyclerViewBindingAdapter.kt
+++ app/src/main/java/com/erman/usurf/directory/ui/DirectoryRecyclerViewBindingAdapter.kt
@@ -10,17 +10,22 @@ import com.erman.usurf.R
 import com.erman.usurf.directory.utils.THUMBNAIL_IMAGE_HEIGHT
 import com.erman.usurf.directory.utils.THUMBNAIL_IMAGE_WIDTH
 import com.erman.usurf.directory.utils.THUMBNAIL_SIZE_MULTIPLIER
+import com.erman.usurf.preference.data.PreferenceProvider
 import java.io.File
 
 @BindingAdapter("filePath")
 fun setImage(imageView: ImageView, filePath: String) {
     val file = File(filePath)
-    if (file.isFile) {
+    
+    if (file.isFile && PreferenceProvider().getShowThumbnailsPreference()) {
         val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE)
             .format(DecodeFormat.PREFER_RGB_565) //uses less memory than default ARGB_8888
             .override(THUMBNAIL_IMAGE_WIDTH, THUMBNAIL_IMAGE_HEIGHT)
             .placeholder(R.drawable.ic_file)
 
-        Glide.with(imageView.context).load(filePath).thumbnail(THUMBNAIL_SIZE_MULTIPLIER).apply(requestOptions).into(imageView)
-    } else imageView.setImageResource(R.drawable.ic_folder)
+        Glide.with(imageView.context).load(filePath).thumbnail(THUMBNAIL_SIZE_MULTIPLIER)
+            .apply(requestOptions).into(imageView)
+
+    } else if (file.isFile) imageView.setImageResource(R.drawable.ic_file)
+    else imageView.setImageResource(R.drawable.ic_folder)
 }
