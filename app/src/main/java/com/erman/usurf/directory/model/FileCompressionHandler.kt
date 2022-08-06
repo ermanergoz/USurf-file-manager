a1682f55fec952bfdfbf9b3bff9f86d25adb407a
ermanergoz <erman.ergoz@gmail.com>
2022-08-06 16:59:26 +0300

    Refactor compression and extraction with detailed logging

--- app/src/main/java/com/erman/usurf/directory/model/FileCompressionHandler.kt
+++ app/src/main/java/com/erman/usurf/directory/model/FileCompressionHandler.kt
@@ -1,31 +1,50 @@
 package com.erman.usurf.directory.model
 
+import com.erman.usurf.utils.logd
+import com.erman.usurf.utils.loge
 import com.hzy.libp7zip.P7ZipApi
 
-/*
-              0      Normal (no errors or warnings detected)
-              1      Warning (Non fatal error(s)). For example, some files cannot be read during compressing. So they were not
-                     compressed
-              2      Fatal error
-              7      Bad command line parameters
-              8      Not enough memory for operation
-              255    User stopped the process with control-C (or similar)
-*/
-
 class FileCompressionHandler {
-    fun compress(compressedFileDirectory: String, sourceDirectory: String, archiveType: String): Boolean {
-        val res = P7ZipApi.executeCommand("7z a -t$archiveType -mx=9 $compressedFileDirectory $sourceDirectory")
+    fun compress(compressedFileDirectory: String, multipleSelection: MutableList<FileModel>, archiveType: String): Boolean {
+        var filesToBeCompressed = ""
+        for (file in multipleSelection) {
+            filesToBeCompressed = filesToBeCompressed + " '" + file.path + "'"
+        }
 
-        if (res == 0 || res == 1)
-            return true
+        when (P7ZipApi.executeCommand("7z a -t$archiveType -mx=9 '$compressedFileDirectory' $filesToBeCompressed")) {
+            0 -> {
+                logd("No errors or warnings detected")
+                return true
+            }
+            1 -> {
+                logd("Warning (Non fatal error(s))")
+                return true
+            }
+            2 -> loge("Fatal error")
+            7 -> loge("Bad command line parameters")
+            8 -> loge("Not enough memory for operation")
+            255 -> loge("The process has been stopped")
+        }
+
         return false
     }
 
     fun extract(compressedFileDirectory: String, outputDirectory: String): Boolean {
-        val res = P7ZipApi.executeCommand("7z x $compressedFileDirectory -o$outputDirectory")
+        when (P7ZipApi.executeCommand("7z x '$compressedFileDirectory' '-o$outputDirectory'")) {
+            0 -> {
+                logd("No errors or warnings detected")
+                return true
+            }
+            1 -> {
+                logd("Warning (Non fatal error(s))")
+                return true
+            }
+            2 -> loge("Fatal error")
+            7 -> loge("Bad command line parameters")
+            8 -> loge("Not enough memory for operation")
+            255 -> loge("The process has been stopped")
+        }
 
-        if (res == 0 || res == 1)
-            return true
         return false
     }
 }
