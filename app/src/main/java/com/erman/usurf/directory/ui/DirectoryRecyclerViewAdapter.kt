73217b1fe531769bd974cf48458476b442413747
ermanergoz <erman.ergoz@gmail.com>
2022-08-16 02:21:43 +0300

    Clean and refactor

--- app/src/main/java/com/erman/usurf/directory/ui/DirectoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/erman/usurf/directory/ui/DirectoryRecyclerViewAdapter.kt
@@ -11,7 +11,6 @@ import com.erman.usurf.directory.utils.MARQUEE_REPEAT_LIM
 import androidx.databinding.library.baseAdapters.BR
 import com.erman.usurf.databinding.RecyclerDirectoryLayoutBinding
 import com.erman.usurf.directory.model.FileModel
-import com.erman.usurf.utils.loge
 
 class DirectoryRecyclerViewAdapter(var viewModel: DirectoryViewModel) :
     RecyclerView.Adapter<DirectoryRecyclerViewAdapter.ViewHolder>() {
--- app/src/main/java/com/erman/usurf/ftp/model/FtpModel.kt
+++ app/src/main/java/com/erman/usurf/ftp/model/FtpModel.kt
@@ -1,28 +1,28 @@
 package com.erman.usurf.ftp.model
 
-import android.content.Context
 import android.content.Intent
-import android.net.wifi.WifiManager
+import android.os.Build
 import androidx.core.content.ContextCompat
 import com.erman.usurf.application.MainApplication.Companion.appContext
 import com.erman.usurf.utils.logd
+import java.net.Inet4Address
+import java.net.NetworkInterface
 
 class FtpModel {
     fun getIpAddress(): String {
         logd("Get ip address")
-        val wifiManager = appContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        //applicationContext is to avoid memory leak
-        val wifiInfo = wifiManager.connectionInfo
-        val ip = wifiInfo.ipAddress
-
-        return "ftps://" + String.format("%d.%d.%d.%d",
-            ip and 0xff, ip shr 8 and 0xff, ip shr 16 and 0xff, ip shr 24 and 0xff)
-        //Formatter.formatIpAddress is deprecated because it doesn't work with ipv6
+        var ip = ""
+        NetworkInterface.getNetworkInterfaces()?.toList()?.map { networkInterface ->
+            networkInterface.inetAddresses?.toList()?.find {
+                !it.isLoopbackAddress && it is Inet4Address
+            }?.let { ip = it.hostAddress?.toString() ?: "" }
+        }
+        return "ftps://$ip"
     }
 
     fun startFTPServer() {
         Intent(appContext, FtpServer()::class.java).also { intent ->
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 ContextCompat.startForegroundService(appContext, intent)
             } else
                 appContext.startService(intent)
