6df7753fbd90527e4df0143c62169557190dd1c7
Yusuf Erman ERGÖZ <yusuf.ergoz@wundermanthompson.com>
2023-07-24 17:11:01 +0200

    Refactor StoragePaths

--- app/src/main/java/com/erman/usurf/utils/StoragePaths.kt
+++ app/src/main/java/com/erman/usurf/utils/StoragePaths.kt
@@ -2,24 +2,46 @@ package com.erman.usurf.utils
 
 import android.annotation.SuppressLint
 import android.os.Build
-import android.os.Environment
 import com.erman.usurf.application.MainApplication
+import com.erman.usurf.directory.utils.ROOT_DIRECTORY
 import com.erman.usurf.preference.data.PreferenceProvider
 import org.koin.java.KoinJavaComponent.getKoin
 import java.io.File
 import java.io.IOException
 
-class StoragePaths {
+object StoragePaths {
     private val preferenceProvider: PreferenceProvider = getKoin().get()
 
     @SuppressLint("SdCardPath")
     fun getStorageDirectories(): Set<String> {
         val paths = mutableSetOf<String>()
+        val storageDirectories = arrayOf(
+            "/data/sdext4",
+            "/data/sdext3",
+            "/data/sdext2",
+            "/data/sdext1",
+            "/data/sdext",
+            "/storage/removable/sdcard1",
+            "/storage/external_SD",
+            "/storage/ext_sd",
+            "/storage/extsd",
+            "/storage/sdcard1",
+            "/storage/sdcard0",
+            "/storage/sdcard",
+            "/mnt/sdcard/external_sd",
+            "/mnt/extsdcard",
+            "/mnt/external_sd",
+            "/mnt/externalsd",
+            "/mnt/emmc",
+            "/mnt/sdcard0",
+            "/mnt/sdcard1",
+            "/mnt/sdcard"
+        )
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            for (file in MainApplication.appContext.getExternalFilesDirs("external")) {
+            for (file in MainApplication.appContext.getExternalFilesDirs(DIR_TYPE_EXTERNAL)) {
                 file?.let {
-                    val index = it.absolutePath.lastIndexOf("/Android/data")
+                    val index = it.absolutePath.lastIndexOf(EXTERNAL_DIR_SUFFIX)
                     var path = it.absolutePath.substring(0, index)
                     try {
                         path = File(path).canonicalPath
@@ -30,84 +52,20 @@ class StoragePaths {
                 }
             }
         }
-
         if (paths.isEmpty() || paths.size == 1) {
-            //Datas
-            if (File("/data/sdext4").exists() && File("/data/sdext4").canRead()) {
-                paths.add("/data/sdext4")
-            }
-            if (File("/data/sdext3").exists() && File("/data/sdext3").canRead()) {
-                paths.add("/data/sdext3")
-            }
-            if (File("/data/sdext2").exists() && File("/data/sdext2").canRead()) {
-                paths.add("/data/sdext2")
-            }
-            if (File("/data/sdext1").exists() && File("/data/sdext1").canRead()) {
-                paths.add("/data/sdext1")
-            }
-            if (File("/data/sdext").exists() && File("/data/sdext").canRead()) {
-                paths.add("/data/sdext")
-            }
-
-            //Storages
-            if (File("/storage/removable/sdcard1").exists() && File("/storage/removable/sdcard1").canRead()) {
-                paths.add("/storage/removable/sdcard1")
-            }
-            if (File("/storage/external_SD").exists() && File("/storage/external_SD").canRead()) {
-                paths.add("/storage/external_SD")
-            }
-            if (File("/storage/ext_sd").exists() && File("/storage/ext_sd").canRead()) {
-                paths.add("/storage/ext_sd")
-            }
-            if (File("/storage/extsd").exists() && File("/storage/extsd").canRead()) {
-                paths.add("/storage/extsd")
-            }
-            if (!(paths.contains("/storage/emulated/0") || paths.contains("/storage/sdcard")) &&
-                File("/storage/sdcard1").exists() && File("/storage/sdcard1").canRead()) {
-                paths.add("/storage/sdcard1")
-            }
-            if (!(paths.contains("/storage/emulated/0") || paths.contains("/storage/sdcard")) &&
-                File("/storage/sdcard0").exists() && File("/storage/sdcard0").canRead()) {
-                paths.add("/storage/sdcard0")
-            }
-            if (File("/storage/sdcard").exists() && File("/storage/sdcard").canRead()) {
-                paths.add("/storage/sdcard")
-            }
-            if (paths.size == 0) {
-                paths.add(Environment.getExternalStorageDirectory().absolutePath)
-            }
-
-            //MNTS
-            if (File("/mnt/sdcard/external_sd").exists() && File("/mnt/sdcard/external_sd").canRead()) {
-                paths.add("/mnt/sdcard/external_sd")
-            }
-            if (File("/mnt/extsdcard").exists() && File("/mnt/extsdcard").canRead()) {
-                paths.add("/mnt/extsdcard")
-            }
-            if (File("/mnt/external_sd").exists() && File("/mnt/external_sd").canRead()) {
-                paths.add("/mnt/external_sd")
-            }
-            if (File("/mnt/externalsd").exists() && File("/mnt/externalsd").canRead()) {
-                paths.add("/mnt/externalsd")
-            }
-            if (File("/mnt/emmc").exists() && File("/mnt/emmc").canRead()) {
-                paths.add("/mnt/emmc")
-            }
-            if (File("/mnt/sdcard0").exists() && File("/mnt/sdcard0").canRead()) {
-                paths.add("/mnt/sdcard0")
-            }
-            if (File("/mnt/sdcard1").exists() && File("/mnt/sdcard1").canRead()) {
-                paths.add("/mnt/sdcard1")
-            }
-            if (!(paths.contains("/storage/emulated/0") || paths.contains("/storage/sdcard")) &&
-                File("/mnt/sdcard").exists() && File("/mnt/sdcard").canRead()) {
-                paths.add("/mnt/sdcard")
+            storageDirectories.forEach {
+                if (File(it).exists() && File(it).canRead()) {
+                    paths.add(it)
+                }
             }
         }
 
-        if (File("/").exists() && File("/").canRead() && preferenceProvider.getRootAccessPreference()) {
-            paths.add("/")
+        if (File(ROOT_DIRECTORY).exists() && File(ROOT_DIRECTORY).canRead() && preferenceProvider.getRootAccessPreference()) {
+            paths.add(ROOT_DIRECTORY)
         }
         return paths
     }
 }
+
+const val DIR_TYPE_EXTERNAL: String = "external"
+const val EXTERNAL_DIR_SUFFIX: String = "/Android/data"
