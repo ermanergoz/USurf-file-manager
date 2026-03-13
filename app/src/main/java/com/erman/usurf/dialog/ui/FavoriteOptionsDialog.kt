08c67674cb1058687dd3a59c77aa0bffcc82849d
Yusuf Erman ERGÖZ <ermanergoz@gmail.com>
2026-03-13 23:54:57 +0300

    Redesign the dialogs

--- app/src/main/java/com/erman/usurf/dialog/ui/FavoriteOptionsDialog.kt
+++ app/src/main/java/com/erman/usurf/dialog/ui/FavoriteOptionsDialog.kt
@@ -1,6 +1,6 @@
 package com.erman.usurf.dialog.ui
 
-import android.app.AlertDialog
+import androidx.appcompat.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
